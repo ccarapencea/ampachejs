ae94474775696e7bccaa9da9c2b70252625cc10b
Constantin Carapencea <Constantin Carapencea>
2017-03-22 22:06:58 +0200

    add express js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var https = require('https');
 var fs = require('fs');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello');
+});
 
 var options = {
   key: fs.readFileSync('config/ssl/key.pem'),
   cert: fs.readFileSync('config/ssl/cert.pem')
 };
 
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(9443);
+https.createServer(options, app).listen(9443, function () {
+  console.log('The server is listening');
+});
