b4efe3c74cd276d5923e28b057ead940cd11b430
Constantin Carapencea <Constantin Carapencea>
2017-03-22 23:04:07 +0200

    send results as xml

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 var https = require('https');
 var fs = require('fs');
 var express = require('express');
+var xml = require('xml');
 
 var app = express();
 
+
 app.get('/', function (req, res) {
   res.send('hello');
+});
+
+app.get('/ampache/server/xml.server.php', function (req, res) {
+  res.set('Content-Type', 'text/xml');
+  res.send(xml({
+    root: [{
+      auth: '1234567890123456789012345678901'
+    }, {
+      version: 1
+    }, {
+      update: new Date().toISOString()
+    }, {
+      add: new Date().toISOString()
+    }, {
+      clean: new Date().toISOString()
+    }, {
+      songs: 0
+    }, {
+      artists: 0
+    }, {
+      albums: 0
+    }, {
+      tags: 0
+    }, {
+      videos: 0
+    }]
+  }));
 });
 
 var options = {
